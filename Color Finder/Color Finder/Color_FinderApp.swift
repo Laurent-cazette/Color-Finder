bc2cde992d719da5834aafd71c404fbb62a670c3
Laurent-cazette <Laurent.cazette@epitech.eu>
2022-09-15 14:38:44 +0200

    added color detection

--- Color Finder/Color Finder/Color_FinderApp.swift
+++ Color Finder/Color Finder/Color_FinderApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Color_FinderApp: App {
     var body: some Scene {
         WindowGroup {
+            SelectColor()
         }
     }
 }
--- Color Finder/Color Finder/SelectColor.swift
+++ Color Finder/Color Finder/SelectColor.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct SelectColor: View {
     @State private var bgColor = Color(.sRGB, red: 0, green: 0, blue: 0)
     @State private var capturedImage: UIImage? = nil
-    let TestImage = UIImage.init(named: "car")
+    var TestImage : UIImage = UIImage(named: "car") ?? UIImage()
+    var croppedPicture : UIImage
+    init() {
+        self.croppedPicture = cropPicture(inputImage: TestImage)!
+    }
     var body: some View {
         NavigationView {
             VStack {
                 ColorPicker("Select the wanted Color", selection: $bgColor)
                     .padding()
                 NavigationLink("Search", destination: ColorDetectionView(capturedImage: $capturedImage))
-                Image(uiImage: cropPicture(inputImage: TestImage!) ?? UIImage())
+                Image(uiImage: croppedPicture)
                     .resizable()
+                    .onAppear {
+                        var red = CGFloat(0)
+                        var green = CGFloat(0)
+                        var blue = CGFloat(0)
+                        var alpha = CGFloat(0)
+                        
+                        croppedPicture.getPixelColor(pos: CGPoint(x: 0, y: 0)).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+                        print(Double(red/255), Double(green/255), Double(blue/255), Double(alpha))
+                    }
             }
         }
         
@@ -37,6 +50,26 @@ func cropPicture(inputImage: UIImage) -> UIImage? {
     let croppedImage: UIImage = UIImage(cgImage: cutImageRef)
     return croppedImage
 }
+
+extension UIImage {
+      func getPixelColor(pos: CGPoint) -> UIColor {
+
+          guard let pixelData = self.cgImage!.dataProvider?.data else {
+              print("An error occured while trying to get the data of the picture")
+              return UIColor(red: 0, green: 0, blue: 0, alpha: 100)
+          }
+          let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+
+          let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+
+          let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+          let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+          let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+          let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+
+          return UIColor(red: r, green: g, blue: b, alpha: a)
+      }
+  }
 
 struct SelectColor_Previews: PreviewProvider {
     static var previews: some View {
